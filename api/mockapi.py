1a884db8d00e015ab9cd542f48a300b188ae06ff
Eric Tamme <eric.tamme@onsip.com>
2018-07-20 14:51:02 -0600

    commenting in some base stuff for the api server

--- api/mockapi.py
+++ api/mockapi.py
@@ -1,11 +1,21 @@
-import SimpleHTTPServer
-import SocketServer
+import SimpleHTTPServer, SocketServer, MySQLdb
+
+#db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+#                     user="user",         # your username
+#                     passwd="pass",  # your password
+#                     db="fdlog")        # name of the data base
+
 
 PORT = 8000
-
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-
 print "serving at port", PORT
 httpd.serve_forever()
+
+
+
+#def check(log_entry):
+#  return ""
+
+#def log(log_entry):
+#  return ""
